92e291839110fb0fbf07765d29314cc86add9734
Siddharth Panda <siddharth.panda@navaratan.com>
2017-12-13 16:09:16 +0530

    home component manual styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  // styleUrls: ['./home.component.scss']
+  styles:[`
+    p { font-weight: bold; }
+    div { color: gray; }
+  `]
 })
 export class HomeComponent implements OnInit {
 
