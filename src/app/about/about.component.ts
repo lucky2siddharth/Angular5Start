af7fa35db5ac93a823198494b5a70ae01bdaf40b
Siddharth Panda <siddharth.panda@navaratan.com>
2017-12-13 18:30:44 +0530

    component navigation by router

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+// component based router
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-about',
@@ -10,11 +12,15 @@ export class AboutComponent implements OnInit {
 
   // create an instance of activatedRoute which we have to do by dependency injection
   //which generally happens in constructor.
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
     this.route.params.subscribe(res => console.log(res.id));
   }
 
   ngOnInit() {
+  }
+
+  sendMeHome() {
+    this.router.navigate([''])
   }
 
 }
