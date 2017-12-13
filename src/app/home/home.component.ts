97b6a137678005832648e6c415fd6ba703f46c32
Siddharth Panda <siddharth.panda@navaratan.com>
2017-12-13 16:32:34 +0530

    Applied styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  // styleUrls: ['./home.component.scss']
-  styles:[`
-    p { font-weight: bold; }
-    div { color: gray; }
-  `]
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
